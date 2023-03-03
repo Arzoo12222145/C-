a5a30e85eaceb886a799beb15de19d56105e6d6b
Arzoo Bhuker <118011789+Arzoo12222145@users.noreply.github.com>
2023-03-03 12:19:12 +0530

    Add files via upload

---
+++ novalueargument.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void add(int,int);
+int main()
+{
+    int a, b;
+    printf("Enter the value of a and b:");
+    scanf("%d%d",&a,&b);
+    add(a,b);
+    return 0;
+}
+void add(int x, int y)
+{
+    int z;
+    z = x + y;
+    printf("\nSum = %d",z);
+}
