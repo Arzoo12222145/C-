570413a15789c613223bd948925239124a6c0ac8
Arzoo Bhuker <118011789+Arzoo12222145@users.noreply.github.com>
2023-04-06 12:29:54 +0530

    Add files via upload

---
+++ structure.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+
+struct student
+{
+    char name[20];
+    int r_no;
+    float fees;
+};
+int main()
+{
+    struct student s1,s2;
+    strcpy(s1.name,"Aniket");
+    s1.r_no = 12;
+    s1.fees = 7800.2;
+    strcpy(s2.name,"Swapnil");
+    s2.r_no = 22;
+    s2.fees = 99.7;
+    printf("\n%s %d %f",s1.name,s1.r_no,s1.fees);
+    printf("\n%s %d %f",s2.name,s2.r_no,s2.fees);
+}
