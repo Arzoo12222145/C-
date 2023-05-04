ad9d9a44250cb04f68cac4a2f3e7ff20be396339
Arzoo Bhuker <118011789+Arzoo12222145@users.noreply.github.com>
2023-05-04 11:16:10 +0530

    Add files via upload

---
+++ pointer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int *p1;
+    char *p2;
+    float *p3;
+    double *p4;
+    printf("\n%d %d %d %d",sizeof(p1),sizeof(p2),sizeof(p3),sizeof(p4));
+    return 0;
+}
